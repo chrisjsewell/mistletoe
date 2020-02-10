4f6a5d7c1a19e2f444c9b849453c1bbf6c3bfa42
Chris Sewell <chrisj_sewell@hotmail.com>
2020-02-11 09:27:47 +1100

    Implement docutils heading->section rendering

--- mistletoe/docutils_renderer.py
+++ mistletoe/docutils_renderer.py
@@ -18,6 +18,7 @@ class DocutilsRenderer(BaseRenderer):
         if self.document is None:     
             self.document = new_document("", settings=None)
         self.current_node = self.document
+        self._level_to_elem = {0: self.document}
         super().__init__(*extras)
 
     def render_children(self, token):
@@ -54,6 +55,62 @@ class DocutilsRenderer(BaseRenderer):
         self.current_node = node
         self.render_children(token)
         self.current_node = current_node
+
+    def render_thematic_break(self, token):
+        self.current_node.append(nodes.transition())
+
+    def render_block_code(self, token):
+        text = token.children[0].content
+        node = nodes.literal_block(text, text, language=token.language, options=token.options)
+        self.current_node.append(node)
+
+    def _is_section_level(self, level, section):
+        return self._level_to_elem.get(level, None) == section
+
+    def _add_section(self, section, level):
+        parent_level = max(
+            section_level for section_level in self._level_to_elem
+            if level > section_level
+        )
+        parent = self._level_to_elem[parent_level]
+        parent.append(section)
+        self._level_to_elem[level] = section
+
+        # Prune level to limit
+        self._level_to_elem = dict(
+            (section_level, section)
+            for section_level, section in self._level_to_elem.items()
+            if section_level <= level
+        )
+
+    def render_heading(self, token):
+        # Test if we're replacing a section level first
+        if isinstance(self.current_node, nodes.section):
+            if self._is_section_level(token.level, self.current_node):
+                self.current_node = self.current_node.parent
+
+        title_node = nodes.title()
+        title_node.line = token.range[0]
+
+        new_section = nodes.section()
+        new_section.line = token.range[0]
+        new_section.append(title_node)
+
+        self._add_section(new_section, token.level)
+
+        current_node = self.current_node
+        self.current_node = title_node
+        self.render_children(token)
+
+        assert isinstance(self.current_node, nodes.title)
+        text = self.current_node.astext()
+        # if self.translate_section_name:
+        #     text = self.translate_section_name(text)
+        name = nodes.fully_normalize_name(text)
+        section = self.current_node.parent
+        section['names'].append(name)
+        self.document.note_implicit_target(section, section)
+        self.current_node = section
 
     def render_link(self, token):
         ref_node = nodes.reference()
@@ -104,11 +161,6 @@ class DocutilsRenderer(BaseRenderer):
         self.render_children(token)
         self.current_node = current_node
 
-    def render_block_code(self, token):
-        text = token.children[0].content
-        node = nodes.literal_block(text, text, language=token.language, options=token.options)
-        self.current_node.append(node)
-
     def render_list(self, token):
         raise NotImplementedError
 
@@ -130,12 +182,6 @@ class DocutilsRenderer(BaseRenderer):
     def render_escape_sequence(self, token):
         raise NotImplementedError
 
-    def render_heading(self, token):
-        raise NotImplementedError
-
-    def render_heading(self, token):
-        raise NotImplementedError
-
     def render_quote(self, token):
         raise NotImplementedError
 
@@ -146,9 +192,6 @@ class DocutilsRenderer(BaseRenderer):
         raise NotImplementedError
 
     def render_table_cell(self, token):
-        raise NotImplementedError
-
-    def render_thematic_break(self, token):
         raise NotImplementedError
 
     def render_line_break(self, token):
